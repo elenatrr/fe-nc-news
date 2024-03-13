b4fb20127bc64cbc8a7f5e80e58a4a306acfeb38
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-03-13 21:17:16 +0000

    save usercontext in localstorage

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,11 +1,22 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 
 const UserContext = createContext();
 
 export const useUser = () => useContext(UserContext);
 
 export const UserProvider = ({ children }) => {
-  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(() => {
+    const savedUser = localStorage.getItem('loggedInUser')
+    return savedUser ? JSON.parse(savedUser) : null
+  });
+
+  useEffect(() => {
+    if (loggedInUser) {
+      localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser))
+    } else {
+      localStorage.removeItem('loggedInUser')
+    }
+  }, [loggedInUser])
 
   return (
     <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>

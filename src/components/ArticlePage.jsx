779719f681aa5b4953799e4f288f20922234e3c5
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-03-14 19:43:16 +0000

    fix non-existent article

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -11,21 +11,33 @@ const ArticlePage = () => {
   const { articleId } = useParams();
   const [article, setArticle] = useState(null);
   const [comments, setComments] = useState([]);
+  const [isNonExistentId, setIsNonExistentId] = useState(false);
 
   useEffect(() => {
+    setIsNonExistentId(false);
+
     fetchArticle(articleId)
       .then((article => {
         setArticle(article);
-      }));
+      }))
+      .catch((err) => {
+        if (err.response.status === 404 ) {
+          setIsNonExistentId(true);
+        }
+      });
   }, []);
 
-  return !article ? <Loader /> : (
-    <div className='article-page'>
-      <ArticleItem article={article} isArticlePreview={false} />
-      <CommentForm setComments={setComments} articleId={articleId}/>
-      <CommentList comments={comments} setComments={setComments} articleId={articleId} commentCount={article?.comment_count}/>
-    </div>
-  );
+  return !article && !isNonExistentId
+    ? <Loader />
+    : isNonExistentId
+      ? <div className="wrapper">Sorry, the selected article &quot;{articleId}&quot; does not exist.</div>
+      : (
+        <div className='article-page'>
+          <ArticleItem article={article} isArticlePreview={false} />
+          <CommentForm setComments={setComments} articleId={articleId}/>
+          <CommentList comments={comments} setComments={setComments} articleId={articleId} commentCount={article?.comment_count}/>
+        </div>
+      );
 };
 
 export default ArticlePage;

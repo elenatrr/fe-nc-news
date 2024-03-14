be336ab5121d9b1b1820b10c5224abecf13cd6c7
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-03-14 13:49:02 +0000

    uninstall extra packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,48 @@
 module.exports = {
-  settings: {
-    react: {
-      version: "detect"
-    }
-  },
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
     },
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"],
-    "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-    eqeqeq: "error",
-    "no-trailing-spaces": "error",
-    "comma-spacing": ["error", { "before": false, "after": true }],
-    "prefer-const": "error",
-    "no-console": "error",
-  },
-};
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"],
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "comma-spacing": ["error", { "before": false, "after": true }],
+      "prefer-const": "error",
+      "no-console": "error",
+    }
+}

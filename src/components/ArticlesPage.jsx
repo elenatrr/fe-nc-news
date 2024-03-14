da115208cb4506e00d587b3ddc8989cc3c1e3937
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-03-14 20:04:51 +0000

    prevent api requests from spamming while selecting topic

--- src/components/ArticlesPage.jsx
+++ src/components/ArticlesPage.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ArticleList from "./ArticleList";
 import TopicList from "./TopicList";
 import { useParams } from "react-router-dom";
 
 const ArticlesPage = () => {
   const { topicName } = useParams();
+  const [areArticlesLoading, setAreArticlesLoading] = useState(false);
 
   return (
     <main className='wrapper'>
-      <TopicList topicName={topicName || null}/>
-      <ArticleList topicName={topicName || null}/>
+      <TopicList topicName={topicName || null} areArticlesLoading={areArticlesLoading}/>
+      <ArticleList topicName={topicName || null} areArticlesLoading={areArticlesLoading} setAreArticlesLoading={setAreArticlesLoading}/>
     </main>
   );
 };

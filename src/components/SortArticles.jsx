c1cb80681f171253542a3e295c98581ac0057b29
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-03-24 11:54:10 +0000

    set url params for orderBy and sortBy

--- src/components/SortArticles.jsx
+++ src/components/SortArticles.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/sort-articles.scss";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const SortArticles = ({ sortBy, setSortBy }) => {
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  useEffect(()=>{
+    const searchParams = new URLSearchParams(location.search)
+
+    searchParams.set("sortBy", sortBy)
+    navigate(`${location.pathname}?${searchParams.toString()}`)
+  }, [sortBy, navigate, location.pathname])
+
   return (
     <ul className='sort'>
       <li className={sortBy === "created_at" ? "sort__item sort__item_clicked" : "sort__item"} onClick={() => setSortBy("created_at")}>new</li>

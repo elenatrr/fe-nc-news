84f08134747e4b1c2d753ea4dc1a76dc69eb7e0a
Elena Tropanets <93614798+elenatrr@users.noreply.github.com>
2024-03-26 19:44:52 +0000

    add info to user profile page

--- src/components/UserProfilePage.jsx
+++ src/components/UserProfilePage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { useUser } from "../contexts/UserContext";
+import "../styles/user-profile.scss";
 
 const UserProfilePage = () => {
+  const { loggedInUser } = useUser();
+
   return (
-    <div>
-      user profile
+    <div className="user-profile">
+      <img className="user-profile__img" src={loggedInUser.avatar_url} alt="User Avatar" />
+      <p className="user-profile__text">{loggedInUser.name}</p>
+      <p className="user-profile__text user-profile__text_blue">@{loggedInUser.username}</p>
     </div>
   );
 };
